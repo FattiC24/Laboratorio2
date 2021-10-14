b50c22582bac9044d09ab6689a5e4cdbc2e55ddf
FattyC24 <62962069+FattyC24@users.noreply.github.com>
2021-10-14 12:23:57 -0600

    lab2

---
+++ app/src/main/java/com/example/laboratorio2/manejar.kt
@@ -0,0 +1,5 @@
+package com.example.laboratorio2
+
+abstract class manejar {
+    abstract open fun manejar()
+}
--- app/src/main/java/com/example/laboratorio2/vehiculo1.kt
+++ app/src/main/java/com/example/laboratorio2/vehiculo1.kt
@@ -1,13 +1,18 @@
 package com.example.laboratorio2
 
-class vehiculo1 (val marca:String, val modelo:String, val anio:Int):vehiculo{
+class vehiculo1(val marca:String, val modelo:String, val anio:Int):manejar(),vehiculo{
 
     fun Listarvehiculo(){
         println("vehiculo $marca, modelo: $modelo, anio: $anio")
         saludo1()
+        manejar()
     }
 
     override fun saludo1() {
-        println("Venihuculo adquidido: $marca")
+        println("Vehiculo adquidido: $marca")
+    }
+
+    override fun manejar() {
+        println("Conduciendo auto $marca $modelo $anio")
     }
 }
